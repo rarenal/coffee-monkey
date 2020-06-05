e79fc6a975a981fedd3b76eaae063c3b7951b954
Glitch (sweltering-pewter-narcissus) <none>
2020-06-05 15:36:39 +0000

    🐡🙎 Support for multiple channels

--- actions.js
+++ actions.js
@@ -2,11 +2,12 @@ const store = require('./store');
 const messages = require('./messages');
 const utils = require('./utils');
 
-exports.accept = async (app, channel, payload) => {
+exports.accept = async (app, payload) => {
   const actionUser = payload.body.actions[0].value;
   const rsvpUser = payload.body.user;
   
   if (rsvpUser.id === actionUser) {
+    const channel = payload.body.channel.id;
     const messageId = payload.body.message.ts;
     const messageBlocks = payload.body.message.blocks;
     const messageBlockId = payload.body.actions[0].block_id;
@@ -22,14 +23,16 @@ exports.accept = async (app, channel, payload) => {
   return payload.ack();
 };
 
-exports.decline = async (app, channel, payload) => {
+exports.decline = async (app, payload) => {
   const actionUser = payload.body.actions[0].value;
   const rsvpUser = payload.body.user;
-  const messageId = payload.body.message.ts;
-  const messageBlocks = payload.body.message.blocks;
-  const messageBlockId = payload.body.actions[0].block_id;
   
   if (rsvpUser.id === actionUser) {
+    const channel = payload.body.channel.id;
+    const messageId = payload.body.message.ts;
+    const messageBlocks = payload.body.message.blocks;
+    const messageBlockId = payload.body.actions[0].block_id;
+
     await app.client.chat.update({
       token: process.env.SLACK_BOT_TOKEN,
       channel,
@@ -41,17 +44,18 @@ exports.decline = async (app, channel, payload) => {
   await payload.ack();
 };
 
-exports.unsubscribe = async (app, channel, users, payload) => {
+exports.unsubscribe = async (app, users, payload) => {
   const rsvpUser = payload.body.actions[0].value;
   const actionUser = payload.body.user;
   
   if (rsvpUser === actionUser.id) {
     store.unsubscribe(rsvpUser);
+    const channel = payload.body.channel.id;
     const messageId = payload.body.message.ts;
     const messageBlocks = payload.body.message.blocks;
     const messageBlockId = payload.body.actions[0].block_id;
     const selectedUsers = messageBlocks.filter((block) => block.type === 'section').map((block) => block.block_id);
-    
+
     const newMember = utils.replaceUser(selectedUsers, users);
         
     await app.client.chat.update({
@@ -65,11 +69,12 @@ exports.unsubscribe = async (app, channel, users, payload) => {
   await payload.ack();
 };
 
-exports.dismiss = async (app, channel, users, payload) => {
+exports.dismiss = async (app, users, payload) => {
   const rsvpUser = payload.body.actions[0].value;
   const actionUser = payload.body.user;
    
   if (rsvpUser === actionUser.id) {
+    const channel = payload.body.channel.id;
     const messageId = payload.body.message.ts;
     const messageBlocks = payload.body.message.blocks;
     const messageBlockId = payload.body.actions[0].block_id;
@@ -88,14 +93,16 @@ exports.dismiss = async (app, channel, users, payload) => {
   await payload.ack();
 };
 
-exports.cancel = async (app, channel, payload) => {
+exports.cancel = async (app, payload) => {
   const actionUser = payload.body.actions[0].value;
   const rsvpUser = payload.body.user;
-  const messageId = payload.body.message.ts;
-  const messageBlocks = payload.body.message.blocks;
-  const messageBlockId = payload.body.actions[0].block_id;
   
   if (rsvpUser.id === actionUser) {
+    const channel = payload.body.channel.id;
+    const messageId = payload.body.message.ts;
+    const messageBlocks = payload.body.message.blocks;
+    const messageBlockId = payload.body.actions[0].block_id;
+
     await app.client.chat.update({
       token: process.env.SLACK_BOT_TOKEN,
       channel,
@@ -106,3 +113,5 @@ exports.cancel = async (app, channel, payload) => {
 
   await payload.ack();
 };
+
+exports.channelFromAction = (payload) => payload.body.channel.id;
--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const actions = require('./actions');
 const messages = require('./messages');
 const utils = require('./utils');
 
-const channel = 'CH34Y9ZPV';
-const numberOfParticipants = 2;
+const runtimeSchedulers = [];
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -20,61 +19,128 @@ const app = new App({
   console.log('⚡️ CofeeMonkey app is running!');
 })();
 
-app.action('accept', async (payload) => {
-  await actions.accept(app, channel, payload);
-});
-
-app.action('decline', async (payload) => {
-  await actions.decline(app, channel, payload);
-});
-
-app.action('unsubscribe', async (payload) => {
-
-  const users = await app.client.conversations.members({
-    token: process.env.SLACK_BOT_TOKEN,
-    channel,
-    limit: 1000
-  });
-  const unsubscribedUsers = store.getUnsubscribed();
-  const eligibleMembers = lodash.difference(users.members, unsubscribedUsers);
-  await actions.unsubscribe(app, channel, eligibleMembers, payload);
-});
-
-app.action('dismiss', async (payload) => {
-  const users = await app.client.conversations.members({
-    token: process.env.SLACK_BOT_TOKEN,
-    channel,
-    limit: 1000
-  });
-  const unsubscribedUsers = store.getUnsubscribed();
-  const eligibleMembers = lodash.difference(users.members, unsubscribedUsers);
-  await actions.dismiss(app, channel, eligibleMembers, payload);
-});
-
-app.action('cancel', async (payload) => {
-  await actions.cancel(app, channel, payload);
-});
-
-//scheduler.scheduleJob("* * * * *", async () => {
-(async () => {
-  const users = await app.client.conversations.members({
-    token: process.env.SLACK_BOT_TOKEN,
-    channel,
-    limit: 1000
-  });
-  
-  if (users.members.length < numberOfParticipants) {
-    return;
+const createScheduler = async (channel, schedule, numberOfMembers) => {
+  const existingSchedulerIndex = runtimeSchedulers.findIndex((runtimeScheduler) => runtimeScheduler.channel === channel);
+  if (existingSchedulerIndex !== -1) {
+    runtimeSchedulers[existingSchedulerIndex].scheduler.cancel();
+    runtimeSchedulers.splice(existingSchedulerIndex, 1);
   }
+
+  const newScheduler = await scheduler.scheduleJob(schedule, async () => {
+      const users = await app.client.conversations.members({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel,
+        limit: 1000
+      });
+
+      if (users.members.length < numberOfMembers) {
+        return;
+      }
+
+      const unsubscribedUsers = store.getUnsubscribed();
+      const eligibleMembers = lodash.difference(users.members, unsubscribedUsers);
+      const randomMembers = utils.getUniqueRandomMembers(numberOfMembers, eligibleMembers);
+
+      app.client.chat.postMessage(messages.meetingMessage(channel, randomMembers));
+  });
+
+  const runtimeScheduler = {
+    scheduler: newScheduler,
+    channel: channel
+  }
+  runtimeSchedulers.push(runtimeScheduler);
+
+}
+
+// Commands API
+
+app.command('/monkey', async ({ command, ack, say, body }) => {
+  await ack();
+  const channel = command.channel_id;
+  const text = body.text;
   
-  const unsubscribedUsers = store.getUnsubscribed();
-  const eligibleMembers = lodash.difference(users.members, unsubscribedUsers);
-  console.log(eligibleMembers);
-  const randomMembers = utils.getUniqueRandomMembers(numberOfParticipants, eligibleMembers);
+  if (text.includes('add')) {
+    try {
+    const result = await app.client.views.open({
+      token: process.env.SLACK_BOT_TOKEN,
+      // Pass a valid trigger_id within 3 seconds of receiving it
+      trigger_id: body.trigger_id,
+      // View payload
+      view: messages.messageSetup(channel),
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+    
+    
+  } else if (text.includes('remove')) {
+    store.removeScheduler(channel);
+    say(`CoffeeMonkey has been removed from this channel by request of <@${body.user_id}>`);
+    
+    const runtimeScheduler = runtimeSchedulers.find((runtimeScheduler) => runtimeScheduler.channel === channel);
+    if (runtimeScheduler) {
+      runtimeScheduler.scheduler.cancel();
+    }    
+  }
+});
+
+// Views API
+
+app.view('set_up', async ({view, body, ack}) => {
+  await ack();
+
+  const numberOfMembers = view.state.values.number_of_participants['participants-value'].selected_option.value;
+  const dayOfWeek = view.state.values.day_of_the_week['day-value'].selected_option.value;
+  const hour = view.state.values.hour_of_the_day['hour-value'].selected_option.value;
   
-  app.client.chat.postMessage(messages.meetingMessage(channel, randomMembers));
-})();
+  const schedule = {hour, dayOfWeek, minute: 0};
+
+  store.addScheduler(view.private_metadata, schedule, parseInt(numberOfMembers));
+  
+  await createScheduler(view.private_metadata, schedule, parseInt(numberOfMembers));
+  
+  app.client.chat.postMessage({
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: view.private_metadata,
+      text: `CoffeeMonkey has been added to this channel by request of <@${body.user.id}>`
+  });
+});
+
+// Events API
 
 app.event('member_joined_channel', ({ event, say }) => {
   say(`Hello world :coffee:🐵! and welcome our new member <@${event.user}>!:tada:`);
 });
+
+// Actions API
+
+app.action('accept', async (payload) => {
+  await actions.accept(app, payload);
+});
+
+app.action('decline', async (payload) => {
+  await actions.decline(app, payload);
+});
+
+app.action('unsubscribe', async (payload) => {
+  const eligibleMembers = await utils.getElegibleMembers(app, actions.channelFromAction(payload));
+  await actions.unsubscribe(app, eligibleMembers, payload);
+});
+
+app.action('dismiss', async (payload) => {
+  const eligibleMembers = await utils.getElegibleMembers(app, actions.channelFromAction(payload));
+  await actions.dismiss(app, eligibleMembers, payload);
+});
+
+app.action('cancel', async (payload) => {
+  await actions.cancel(app, payload);
+});
+
+// Schedulers
+(async () => {
+  store.getSchedulers().forEach(async (job) => {
+    console.log(job);
+    await createScheduler(job.channel, job.schedule, job.members);
+  });
+})();
